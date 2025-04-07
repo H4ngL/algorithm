324e86f5135940b27b0d5d9532be489f9dcb33e2
Hangyeol Kim <102140830+H4ngL@users.noreply.github.com>
2025-04-07 16:56:11 +0900

    [Gold III] Title: 가장높은탑쌓기, Time: 188 ms, Memory: 18352 KB -BaekjoonHub

--- 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.java
+++ 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.java
@@ -1,67 +1,69 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static class Brick {
-		int id, width, height, weight;
 
-		Brick(int id, int width, int height, int weight) {
-			this.id = id;
-			this.width = width;
-			this.height = height;
-			this.weight = weight;
-		}
-	}
+    static class Brick {
+        int index;
+        int area;
+        int height;
+        int weight;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(in.readLine());
+        Brick(int index, int area, int height, int weight) {
+            this.index = index;
+            this.area = area;
+            this.height = height;
+            this.weight = weight;
+        }
+    }
 
-		Brick[] bricks = new Brick[n + 1];
-		int[] dp = new int[n + 1]; // dp[i] : i번째 벽돌을 맨 위에 둘 때 최대 높이
-		int[] path = new int[n + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Brick[] bricks = new Brick[n];
 
-		bricks[0] = new Brick(0, 0, 0, 0);
-		for (int i = 1; i <= n; i++) {
-			StringTokenizer st = new StringTokenizer(in.readLine());
-			int width = Integer.parseInt(st.nextToken());
-			int height = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			bricks[i] = new Brick(i, width, height, weight);
-		}
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int area = Integer.parseInt(st.nextToken());
+            int height = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            bricks[i] = new Brick(i + 1, area, height, weight);
+        }
 
-		// 내림차순 정렬
-		Arrays.sort(bricks, 1, n + 1, (a, b) -> b.width - a.width);
+        Arrays.sort(bricks, (b1, b2) -> b2.area - b1.area);
 
-		int maxHeight = 0;
-		int maxIndex = 0;
+        int[] dp = new int[n];      
+        int[] trace = new int[n];  
+        Arrays.fill(trace, -1);
 
-		for (int i = 1; i <= n; i++) {
-			dp[i] = bricks[i].height;
-			for (int j = 1; j < i; j++) {
-				if (bricks[j].weight > bricks[i].weight) {
-					if (dp[i] < dp[j] + bricks[i].height) {
-						dp[i] = dp[j] + bricks[i].height;
-						path[i] = j; // i번 벽돌 아래에는 j번 벽돌
-					}
-				}
-			}
+        int maxHeight = 0;
+        int lastIndex = 0;
 
-			if (maxHeight < dp[i]) {
-				maxHeight = dp[i];
-				maxIndex = i;
-			}
-		}
+        for (int i = 0; i < n; i++) {
+            dp[i] = bricks[i].height;
+            for (int j = 0; j < i; j++) {
+                if (bricks[j].weight > bricks[i].weight && dp[j] + bricks[i].height > dp[i]) {
+                    dp[i] = dp[j] + bricks[i].height;
+                    trace[i] = j;
+                }
+            }
+            if (dp[i] > maxHeight) {
+                maxHeight = dp[i];
+                lastIndex = i;
+            }
+        }
 
-		Deque<Integer> q = new ArrayDeque<>();
-		while (maxIndex != 0) {
-			q.add(bricks[maxIndex].id);
-			maxIndex = path[maxIndex];
-		}
+        // 경로 역추적
+        LinkedList<Integer> result = new LinkedList<>();
+        while (lastIndex != -1) {
+            result.add(bricks[lastIndex].index);
+            lastIndex = trace[lastIndex];
+        }
 
-		System.out.println(q.size());
-		while (!q.isEmpty()) {
-			System.out.println(q.pop());
-		}
-	}
+        // 출력
+        System.out.println(result.size());
+        for (int num : result) {
+            System.out.println(num);
+        }
+    }
 }
