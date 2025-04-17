c01e73a70e4dda65f0b3ba5284879ac4d0d6acfd
Hangyeol Kim <102140830+H4ngL@users.noreply.github.com>
2025-04-17 22:44:10 +0900

    [Silver II] Title: 비슷한 단어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+string standard;
+unordered_map<char, int> map;
+
+bool isSimilar(string str)
+{
+    int len1 = standard.length();
+    int len2 = str.length();
+
+    if (abs(len1 - len2) > 1)
+        return false;
+
+    unordered_map<char, int> copied = map;
+    unordered_map<char, int> curMap;
+    for (char c : str)
+        curMap[c]++;
+
+    int diff = 0;
+    for (char c = 'A'; c <= 'Z'; c++)
+    {
+        diff += abs(copied[c] - curMap[c]);
+    }
+
+    return diff <= 1 || (diff == 2 && len1 == len2);
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    cin >> standard;
+    for (char c : standard)
+        map[c]++;
+
+    int cnt = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        string str;
+        cin >> str;
+        if (isSimilar(str))
+            cnt++;
+    }
+
+    cout << cnt << "\n";
+
+    return 0;
+}
