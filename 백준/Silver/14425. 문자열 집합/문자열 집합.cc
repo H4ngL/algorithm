42f860ea74de959a642b9183bf2f51e6a8ef73fd
Hangyeol Kim <102140830+H4ngL@users.noreply.github.com>
2025-09-19 21:31:47 +0900

    [Silver IV] Title: 문자열 집합, Time: 52 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int n, m;
+  cin >> n >> m;
+  
+  unordered_map<string, bool> map;
+  
+  for(int i = 0; i < n; i++) {
+    string str;
+    cin >> str;
+    map[str] = true;
+  }
+  
+  int cnt = 0;
+  for(int i = 0; i < m; i++) {
+    string str;
+    cin >> str;
+    if(map[str]) cnt++;
+  }
+  
+  cout << cnt;
+  
+  return 0;
+}
