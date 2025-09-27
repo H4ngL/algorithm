c03ef28bb0cdc0f7b02a4514e37ee08529c5ded5
Hangyeol Kim <102140830+H4ngL@users.noreply.github.com>
2025-09-27 23:11:08 +0900

    [Bronze IV] Title: 샤틀버스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  cin >> x >> y;
+  
+  if(x > y) cout << x + y << '\n';
+  else if(x < y) cout << y - x << '\n';
+  else cout << 0 << '\n';
+  
+  return 0;
+}
